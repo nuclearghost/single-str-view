9775a133abbc7a24a0b13578bd9d0a500acc0a0d
Mark Meyer <mark.meyer37@gmail.com>
2015-12-21 16:11:16 -0600

    Initial Commit

---
+++ SingleViewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleViewApp
+//
+//  Created by Mark Meyer on 12/21/15.
+//  Copyright © 2015 Sharethrough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleViewApp
+//
+//  Created by Mark Meyer on 12/21/15.
+//  Copyright © 2015 Sharethrough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

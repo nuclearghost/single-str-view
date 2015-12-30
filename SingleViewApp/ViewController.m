59b4640929c55e30f364e00ad0ffcee4cf9a5cf9
Mark Meyer <mark.meyer37@gmail.com>
2015-12-30 12:32:39 -0600

    Add methods for loading and clearing ads

--- SingleViewApp/ViewController.m
+++ SingleViewApp/ViewController.m
@@ -7,46 +7,49 @@
 //
 
 #import "ViewController.h"
-#import <SharethroughSDK+DFP/SharethroughSDK+DFP.h>
+//#import <SharethroughSDK+DFP/SharethroughSDK+DFP.h>
+#import <SharethroughSDK+DFP/SharethroughSDK.h>
 
-@interface AdDelegate : NSObject <STRAdViewDelegate>
+//@interface AdDelegate : NSObject <STRAdViewDelegate>
+//
+//+ (id)sharedInstance;
+//
+//@end
+//
+//@implementation AdDelegate
+//
+//+ (instancetype)sharedInstance {
+//    __strong static AdDelegate *testSafeSharedObject = nil;
+//
+//    static dispatch_once_t p = 0;
+//    dispatch_once(&p, ^{
+//        testSafeSharedObject = [[self alloc] init];
+//    });
+//
+//    return testSafeSharedObject;
+//}
+//
+//#pragma mark - <STRAdViewDelegate>
+//
+//- (void)didPrefetchAdvertisement:(STRAdvertisement *)strAd {
+//    NSLog(@"didPrefetch");
+//}
+//
+//- (void)didFailToPrefetchForPlacementKey:(NSString *)placementKey {
+//    NSLog(@"failed to prefetch");
+//}
+//
+//- (void)adView:(id<STRAdView>)adView didFetchAdForPlacementKey:(NSString *)placementKey atIndex:(NSInteger)adIndex {
+//    NSLog(@"didFetch");
+//}
+//
+//- (void)adView:(id<STRAdView>)adView didFailToFetchAdForPlacementKey:(NSString *)placementKey atIndex:(NSInteger)adIndex{
+//    NSLog(@"didFail");
+//}
+//
+//@end
 
-+ (id)sharedInstance;
-
-@end
-
-@implementation AdDelegate
-
-+ (instancetype)sharedInstance {
-    __strong static AdDelegate *testSafeSharedObject = nil;
-
-    static dispatch_once_t p = 0;
-    dispatch_once(&p, ^{
-        testSafeSharedObject = [[self alloc] init];
-    });
-
-    return testSafeSharedObject;
-}
-
-#pragma mark - <STRAdViewDelegate>
-
-- (void)didPrefetchAdvertisement:(STRAdvertisement *)strAd {
-    NSLog(@"didPrefetch");
-}
-
-- (void)didFailToPrefetchForPlacementKey:(NSString *)placementKey {
-    NSLog(@"failed to prefetch");
-}
-
-- (void)adView:(id<STRAdView>)adView didFetchAdForPlacementKey:(NSString *)placementKey atIndex:(NSInteger)adIndex {
-    NSLog(@"didFetch");
-}
-
-- (void)adView:(id<STRAdView>)adView didFailToFetchAdForPlacementKey:(NSString *)placementKey atIndex:(NSInteger)adIndex{
-    NSLog(@"didFail");
-}
-
-@end
+static NSString *kPlacementKey = @"e7244b42";
 
 @interface ViewController () <STRAdViewDelegate>
 
@@ -63,7 +66,8 @@
 #endif
     
     // Do any additional setup after loading the view, typically from a nib.
-    [[SharethroughSDKDFP sharedInstance] prefetchAdForPlacementKey:@"e7244b42" delegate:self];
+//    [[SharethroughSDK sharedInstance] prefetchAdForPlacementKey:kPlacementKey delegate:[AdDelegate sharedInstance]];
+//    [[SharethroughSDK sharedInstance] prefetchAdForPlacementKey:@"f8b11995" delegate:self];
 }
 
 -(void)orientationDidChange:(NSNotification *)notification{
@@ -72,6 +76,7 @@
 
 - (void)didPrefetchAdvertisement:(STRAdvertisement *)strAd {
     NSLog(@"didPrefetch");
+    [self assignAllAds];
 }
 
 - (void)didFailToPrefetchForPlacementKey:(NSString *)placementKey {
@@ -93,5 +98,31 @@
 
 - (IBAction)popViewController:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)fetchAds:(id)sender {
+    [[SharethroughSDK sharedInstance] prefetchAdForPlacementKey:kPlacementKey delegate:self];
+}
+
+- (NSInteger)numberOfAdsAvialable {
+    NSInteger nAds = [[SharethroughSDK sharedInstance] totalNumberOfAdsAvailableForPlacement:kPlacementKey];
+    NSLog(@"There are %ld ads available", (long)nAds);
+    return nAds;
+}
+
+- (void)assignAllAds {
+    NSInteger nAds = [self numberOfAdsAvialable];
+    for (int i = 0; i < nAds; ++i) {
+        [[SharethroughSDK sharedInstance] AdForPlacement:kPlacementKey atIndex:i];
+    }
+    NSInteger nUnassignedAds = [[SharethroughSDK sharedInstance] unassignedNumberOfAdsAvailableForPlacement:kPlacementKey];
+    NSLog(@"Assigned %ld ads. %ld unassigned", (long)nAds, (long)nUnassignedAds);
+    [self clearAds];
+}
+
+- (void)clearAds {
+    [[SharethroughSDK sharedInstance] clearCachedAdsForPlacement:kPlacementKey];
+    NSInteger nAds = [self numberOfAdsAvialable];
+    NSLog(@"Expected %ld to be 0", (long)nAds);
 }
 @end
